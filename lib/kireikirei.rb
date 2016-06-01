192a8b409854d1cd112167d400a37a643390e9f1
timakin <timaki.st@gmail.com>
2016-06-01 23:35:50 +0900

    init

---
+++ lib/kireikirei.rb
@@ -0,0 +1,26 @@
+require "kireikirei/version"
+require 'json'
+
+module Kireikirei
+  def execute(opt)
+    if opt == '-a'
+      paths = Dir.glob("./**/*.json")
+    else
+      Dir.chdir("./")
+      cmd = `git diff --name-only`
+      diff_arr = cmd.split(/$/).map(&:strip)
+      paths = diff_arr
+    end
+
+    for path in paths
+      begin
+        file = File.read(path)
+        data_hash = JSON.parse(file)
+        formatted = JSON.pretty_generate(data_hash).gsub /^$\n/, ''
+        File.write(path, formatted)
+      rescue
+        next
+      end
+    end
+  end
+end
---
+++ lib/kireikirei/version.rb
@@ -0,0 +1,3 @@
+module Kireikirei
+  VERSION = "0.1.0"
+end
