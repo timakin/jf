74627abe0ca77aedfbe55f60f0bd50f66f93c306
timakin <timaki.st@gmail.com>
2016-06-04 23:24:21 +0900

    stdin reading, refactoring

--- lib/kireikirei.rb
+++ lib/kireikirei.rb
@@ -1,33 +1,65 @@
 require "kireikirei/version"
 require 'json'
 
+class FileParseError < StandardError; end
+
 module Kireikirei
-  def execute(target, opts)
-    if File.exists?(target)
+  def self.execute(target, opts)
+    if target.is_stdin
+      parse_stdin(target.body, opts)
+      return
+    end
+
+    if File.exists?(target.body)
       if opts["git-diff"]
-        cmd = `git status | egrep "$*.json" | sed -E "s/([^:]+: +)(.*)/\\2/"`
+        cmd = list_diff_json
         diff_arr = cmd.split(/$/).map(&:strip)
         paths = diff_arr
       else
-        paths = File.directory?(target) ? Dir.glob("#{target}/**/*.json") : Dir.glob(target)
+        paths = File.directory?(target.body) ? Dir.glob("#{target.body}/**/*.json") : Dir.glob(target.body)
       end
     else
-      puts "[Error] Could not find directory ot file."
+      assert_file_not_found
       exit
     end
 
     for path in paths
-      next if /.+.json/ !~ path
+      next if is_not_json
       begin
-        file = File.read(path)
-        data_hash = JSON.parse(file.force_encoding("UTF-8"))
-        formatted = JSON.pretty_generate(data_hash).gsub /^$\n/, ''
-        File.write(path, formatted)
-      rescue => e
+        rewrite(path, opts)
+      rescue FileParseError => e
+        assert_parse_failed
         next
       end
     end
   end
 
-  module_function :execute
+  def self.rewrite(path, opts)
+    file = File.read(path)
+    data_hash = JSON.parse(file.force_encoding("UTF-8"))
+    formatted = JSON.pretty_generate(data_hash).gsub /^$\n/, ''
+    File.write(path, formatted)
+  end
+
+  def self.parse_stdin(stdin, otps)
+    data_hash = JSON.parse(stdin)
+    formatted = JSON.pretty_generate(data_hash).gsub /^$\n/, ''
+    puts formatted
+  end
+
+  def self.is_not_json(path)
+    return /.+.json/ !~ path
+  end
+
+  def self.list_diff_json
+    `git status | egrep "$*.json" | sed -E "s/([^:]+: +)(.*)/\\2/"`
+  end
+
+  def self.assert_file_not_found
+    puts "[Error] Could not find directory ot file."
+  end
+
+  def self.assert_parse_failed
+    puts "[Error] Could not parse json correctly."
+  end
 end
