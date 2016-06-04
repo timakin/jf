526c7903938244029af732ef2e100b6d3bf44563
timakin <timaki.st@gmail.com>
2016-06-04 21:30:57 +0900

    shift to next loop unless json

--- lib/kireikirei.rb
+++ lib/kireikirei.rb
@@ -1,8 +1,6 @@
 require "kireikirei/version"
 require 'json'
 
-
-
 module Kireikirei
   def execute(target, opts)
     if File.exists?(target)
@@ -19,14 +17,15 @@ module Kireikirei
     end
 
     for path in paths
-        begin
-            file = File.read(path)
-            data_hash = JSON.parse(file.force_encoding("UTF-8"))
-            formatted = JSON.pretty_generate(data_hash).gsub /^$\n/, ''
-            File.write(path, formatted)
-        rescue => e
-            next
-        end
+      next if /.+.json/ !~ path
+      begin
+        file = File.read(path)
+        data_hash = JSON.parse(file.force_encoding("UTF-8"))
+        formatted = JSON.pretty_generate(data_hash).gsub /^$\n/, ''
+        File.write(path, formatted)
+      rescue => e
+        next
+      end
     end
   end
 
