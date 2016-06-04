d4eae59b8b31874399d2816e868ef3ebc4f755b0
timakin <timaki.st@gmail.com>
2016-06-04 14:45:44 +0900

    update

--- lib/kireikirei.rb
+++ lib/kireikirei.rb
@@ -1,26 +1,32 @@
 require "kireikirei/version"
 require 'json'
 
+
+
 module Kireikirei
-  def execute(opt)
-    if opt == '-a'
-      paths = Dir.glob("./**/*.json")
+  def execute(target, opts)
+    if File.exists?(target)
+      if opts["git-diff"]
+        cmd = `git status | egrep "$*.json" | sed -E "s/([^:]+: +)(.*)/\\2/"`
+        diff_arr = cmd.split(/$/).map(&:strip)
+        paths = diff_arr
+      else
+        paths = File.directory?(target) ? Dir.glob("#{target}/**/*.json") : Dir.glob(target)
+      end
     else
-      Dir.chdir("./")
-      cmd = `git diff --name-only`
-      diff_arr = cmd.split(/$/).map(&:strip)
-      paths = diff_arr
+      puts "[Error] Could not find directory ot file."
+      exit
     end
 
     for path in paths
-      begin
-        file = File.read(path)
-        data_hash = JSON.parse(file)
-        formatted = JSON.pretty_generate(data_hash).gsub /^$\n/, ''
-        File.write(path, formatted)
-      rescue
-        next
-      end
+        begin
+            file = File.read(path)
+            data_hash = JSON.parse(file.force_encoding("UTF-8"))
+            formatted = JSON.pretty_generate(data_hash).gsub /^$\n/, ''
+            File.write(path, formatted)
+        rescue => e
+            next
+        end
     end
   end
 
