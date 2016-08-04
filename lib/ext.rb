a10c517144c372f9feb92c7d7342a5e4800fade9
timakin <timaki.st@gmail.com>
2016-08-05 08:16:26 +0900

    [add] String option, key.one? conditions

--- lib/ext.rb
+++ lib/ext.rb
@@ -1,74 +1,85 @@
 class String
-  def numstr_to_i
-    is_int?(self) ? self.to_i : self
-  end
+    def numstr_to_i
+        is_int?(self) ? to_i : self
+    end
 
-  def is_int?(str)
-    Integer(str)
-    true
-  rescue ArgumentError
-    false
-  end
+    def is_int?(str)
+        Integer(str)
+        true
+    rescue ArgumentError
+        false
+    end
 end
 
 class Array
-  def one?
-    self.size == 1
-  end
+    def one?
+        size == 1
+    end
 end
 
 class Hash
-  def slice(*keys)
-    keys.map! { |key| convert_key(key) } if respond_to?(:convert_key, true)
-    keys.each_with_object(self.class.new) { |k, hash| hash[k] = self[k] if has_key?(k) }
-  end
-
-  def slice!(*keys)
-    keys.map! { |key| convert_key(key) } if respond_to?(:convert_key, true)
-    omit = slice(*self.keys - keys)
-    hash = slice(*keys)
-    hash.default      = default
-    hash.default_proc = default_proc if default_proc
-    replace(hash)
-    omit
-  end
-
-  def extract!(*keys)
-    keys.each_with_object(self.class.new) { |key, result| result[key] = delete(key) if has_key?(key) }
-  end
-
-  def insert_after(key, obj)
-    splitter = self.keys.index(key)
-    before_keys = self.keys[0..splitter]
-    before = self.extract!(*before_keys)
-    after = self.slice!(*before_keys)
-    merged = before.merge(obj).merge(after)
-    merged
-  end
-
-  def deep_merge(*keys, val)
-    merge_target = keys.inject(self) { |h, k| h[k.numstr_to_i] }
-    if keys.one?
-      self[keys.last] = merge_target.merge(val)
-    else
-      if merge_target.kind_of?(Array)
-        merge_to_array(*keys, merge_target, val)
-      else
-        merge_to_hash(*keys, merge_target, val)
-      end
+    def slice(*keys)
+        keys.map! { |key| convert_key(key) } if respond_to?(:convert_key, true)
+        keys.each_with_object(self.class.new) { |k, hash| hash[k] = self[k] if key?(k) }
     end
-    self
-  end
 
-  private
+    def slice!(*keys)
+        keys.map! { |key| convert_key(key) } if respond_to?(:convert_key, true)
+        omit = slice(*self.keys - keys)
+        hash = slice(*keys)
+        hash.default      = default
+        hash.default_proc = default_proc if default_proc
+        replace(hash)
+        omit
+    end
 
-  def merge_to_array(*keys, target, val)
-    last_key = keys.pop
-    keys.inject(self) { |h, k| h[k.numstr_to_i] }[last_key.numstr_to_i] = target.push(val)
-  end
+    def extract!(*keys)
+        keys.each_with_object(self.class.new) { |key, result| result[key] = delete(key) if key?(key) }
+    end
 
-  def merge_to_hash(*keys, target, val)
-    last_key = keys.pop
-    keys.inject(self) { |h, k| h[k.numstr_to_i] }[last_key.numstr_to_i] = target.merge(val)
-  end
+    def insert_after(key, obj)
+        splitter = keys.index(key)
+        before_keys = keys[0..splitter]
+        before = extract!(*before_keys)
+        after = slice!(*before_keys)
+        merged = before.merge(obj).merge(after)
+        merged
+    end
+
+    def deep_merge(*keys, val)
+        merge_target = keys.inject(self) { |h, k| h[k.numstr_to_i] }
+        if keys.one?
+            self[keys.last] = if self[keys.last].is_a?(Hash)
+                                  merge_target.merge(val)
+                              else
+                                  val
+                              end
+        else
+            if merge_target.is_a?(Array)
+                merge_to_array(*keys, merge_target, val)
+            elsif merge_target.is_a?(Hash)
+                merge_to_hash(*keys, merge_target, val)
+            else # if merge_target is a kind of String
+                replace(*keys, merge_target, val)
+            end
+        end
+        self
+    end
+
+    private
+
+    def merge_to_array(*keys, target, val)
+        last_key = keys.pop
+        keys.inject(self) { |h, k| h[k.numstr_to_i] }[last_key.numstr_to_i] = target.push(val)
+    end
+
+    def merge_to_hash(*keys, target, val)
+        last_key = keys.pop
+        keys.inject(self) { |h, k| h[k.numstr_to_i] }[last_key.numstr_to_i] = target.merge(val)
+    end
+
+    def replace(*keys, _target, val)
+        last_key = keys.pop
+        keys.inject(self) { |h, k| h[k.numstr_to_i] }[last_key.numstr_to_i] = val
+    end
 end
